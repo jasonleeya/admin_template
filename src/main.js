d37740b111c49ea0fa3cb9036049c7ee9de86795
JasonLee <1615685977@qq.com>
2019-05-23 19:43:02 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/styles/reset.css'
+import "@/assets/styles/iconfont/iconfont.css"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
